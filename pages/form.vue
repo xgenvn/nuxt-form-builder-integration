2c7bcad3a7d22f53dcaf61624322a06a079424e9
Tu Nguyen <tuna@hybrid-technologies.co.jp>
2020-11-14 13:41:50 +0700

    initial

---
+++ pages/form.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <form-builder :form="form" :config="formConfig"></form-builder>
+  </div>
+</template>
+
+<script>
+import { useForm } from '@fext/vue-use'
+import { FormBuilder } from '@fext/vue-form-builder'
+
+export default {
+  name: 'AwesomeForm',
+
+  components: {
+    FormBuilder,
+  },
+
+  setup() {
+    // Form Composition API
+    const form = useForm()
+    const { formValues, updateFormValues } = form
+
+    return {
+      form,
+
+      /* 表单数据模型 */
+      formValues,
+
+      /* 手动更新表单数据模型 */
+      updateFormValues,
+    }
+  },
+
+  data() {
+    return {
+      formConfig: [
+        {
+          component: 'el-card',
+          props: {
+            header: 'Basic Group',
+          },
+          fields: [
+            {
+              name: 'comment',
+              component: 'ElFormAdaptor',
+              label: 'Description',
+              rules: {
+                required: true,
+              },
+              props: {
+                type: 'textarea',
+                rows: 5,
+                placeholder:
+                  'Use adaptor and render with default(el-input) component',
+              },
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ plugins/vueform.js
@@ -0,0 +1,6 @@
+import ElFormAdaptorPlugin from '@fext/vue-form-builder/lib/adaptor/element'
+import FormBuilderPlugin from '@fext/vue-form-builder'
+import Vue from 'vue'
+
+Vue.use(FormBuilderPlugin)
+Vue.use(ElFormAdaptorPlugin)
